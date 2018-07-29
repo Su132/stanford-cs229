48746f9c126348c51339564668a8501dd4e25633
Wayne Small <waynemystir@gmail.com>
2018-07-29 09:48:58 -0400

    PS2-Q6

--- prob-set-2/Q6-Spam-classification/nb.py
+++ prob-set-2/Q6-Spam-classification/nb.py
@@ -20,14 +20,26 @@ def nb_train(matrix, category):
     state = {}
     N = matrix.shape[1]
     ###################
-
+    X1, X0 = matrix[category==1], matrix[category==0]
+    state['phi_js_yeq1'] = (np.sum(X1, axis=0) + 1) / (np.sum(X1) + N)
+    state['phi_js_yeq0'] = (np.sum(X0, axis=0) + 1) / (np.sum(X0) + N)
+    state['phi_y'] = np.sum(category==1) / category.shape[0]
     ###################
     return state
 
 def nb_test(matrix, state):
     output = np.zeros(matrix.shape[0])
     ###################
+    phi_js_yeq1, phi_js_yeq0, phi_y = state['phi_js_yeq1'], state['phi_js_yeq0'], state['phi_y']
 
+    L1 = np.log(phi_js_yeq1) * matrix # (n,) * (matrix.shape[0], n) = (matrix.shape[0], n)
+    L0 = np.log(phi_js_yeq0) * matrix
+    log_phi_yeq1 = np.sum(L1, axis=1) # (matrix.shape[0],)
+    log_phi_yeq0 = np.sum(L0, axis=1)
+    r = log_phi_yeq0 + np.log(1-phi_y) - log_phi_yeq1 - np.log(phi_y)
+    probs = 1./(1 + np.exp(np.clip(r, -700, 700)))
+    
+    output[probs>.5] = 1
     ###################
     return output
 
